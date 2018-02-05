7cf1da8d9271cd58dda7ab9eaa015138c85150ab
sweeneyb <tab126@gmail.com>
2018-02-04 21:55:37 -0600

    adds another row

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
 INSERT INTO restaurants (name, location, lat, lng) VALUES ('test', 'Helsinki', 123, 456);
 
-INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Soups', 'Hello, World!');
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Soups2', 'Hello, World!');
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads', 'Hello, Salad!');
