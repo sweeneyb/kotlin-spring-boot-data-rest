4fb30af8580c290ccb999533c6dae066fe070146
sweeneyb <tab126@gmail.com>
2018-02-05 00:24:44 -0600

    add entries to get a second page

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,3 +2,31 @@ INSERT INTO restaurants (name, location, lat, lng) VALUES ('test', 'Helsinki', 1
 
 INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Soups2', 'Hello, World!');
 INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads', 'Hello, Salad!');
+
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads3', 'Hello, Salad!');
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads4', 'Hello, Salad!');
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads5', 'Hello, Salad!');
+
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads6', 'Hello, Salad!');
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads7', 'Hello, Salad!');
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads8', 'Hello, Salad!');
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads9', 'Hello, Salad!');
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads10', 'Hello, Salad!');
+
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads11', 'Hello, Salad!');
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads12', 'Hello, Salad!');
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads13', 'Hello, Salad!');
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads14', 'Hello, Salad!');
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads15', 'Hello, Salad!');
+
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads16', 'Hello, Salad!');
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads17', 'Hello, Salad!');
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads18', 'Hello, Salad!');
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads19', 'Hello, Salad!');
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads20', 'Hello, Salad!');
+
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads21', 'Hello, Salad!');
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads22', 'Hello, Salad!');
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads23', 'Hello, Salad!');
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads24', 'Hello, Salad!');
+INSERT INTO menus (restaurant_id, menu_date, menu_date2, title, menu_text) VALUES (1, CURRENT_DATE(), CURRENT_DATE(), 'Salads25', 'Hello, Salad!');
